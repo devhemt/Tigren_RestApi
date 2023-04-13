83b75fd6fe53dd68cb71893218ad81d91a2809c1
Thanh Lich <lichk2002@gmail.com>
2023-04-13 11:53:23 +0700

    Custom Payment

---
+++ app/code/Tigren/RestApi/view/frontend/web/js/view/payment/method-renderer/custompayment.js
@@ -0,0 +1,9 @@
+define(["Magento_Checkout/js/view/payment/default"], function (Component) {
+    "use strict";
+
+    return Component.extend({
+        defaults: {
+            template: "Tigren_RestApi/payment/customtemplate",
+        },
+    });
+});
